6042119eab63203ff3f3ca0da443e9ada22f12f2
Vitaliy <vitalianobro@gmail.com>
2026-06-13 17:12:49 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-import json
-import telebot
 import requests
 import logging
 import os
+import telebot
 
-
-from environs import env
+from google.cloud import dialogflow_v2beta1 as dialogflow
 from dotenv import load_dotenv
 from telebot import types
+from send_message_tg import send_message_tg
+from environs import env
 
 
-load_dotenv()
-token_google = os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "vitalius-project-5a42f95173d3"
-    
-chat_id = env.str("TELEGRAM_CHAT_ID")
-tg_bot_token = env.str('TELEGRAM_BOT_API_KEY')
-bot=telebot.TeleBot(tg_bot_token)
+def main():
+    load_dotenv()
+    token_google = os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:/python_scripts/ChatBot/ChatBot_02/vitalius-project-5a42f95173d3.json'   
+    chat_id = env.str("TELEGRAM_CHAT_ID")
+    tg_bot_token = env.str('TELEGRAM_BOT_API_KEY')
+    bot=telebot.TeleBot(tg_bot_token)
+    project_id = env.str('PROJECT_ID')
+    session_id = env.str('SESSION_ID')
+    text = 'Hi'
+    language_code = 'ru'
+    send_message_tg(bot, session_id, text, language_code, project_id)
 
 
-@bot.message_handler(func=lambda message: message.text in ['start'])
-def start_button_message(message):
-    bot.send_message(
-        message.chat.id,
-        text = "Здравствуйте"
-    )
-
-@bot.message_handler()
-def send_message(message):
-    bot.send_message(message.chat.id, text =message.text)
-
-bot.infinity_polling()
+if __name__ == "__main__":
+    main()
